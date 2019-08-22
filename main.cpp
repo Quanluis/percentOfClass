079b3474d5a7f1e901aa4ff5949b083e5bc75993
Quanluis <quanluis@protonmail.com>
2019-08-22 13:34:05 -0400

    created a simple static percentage program. Will add the ability to recieve user input next.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -6,15 +7,16 @@ int main(){
     // Number of students in a class based on gender.
     // Change values if needed (only change male and female variables)
 
-    int males = 10,
-        females = 10,
-         total = males + females,
-        percentage = (total / males) + 30;
+    double males = 25,
+        females = 25,
+        total = males + females,
+        percentageMales = (float)males / (float)total * 100, 
+        percentageFemales = (float)females / (float)total * 100;
 
     cout << "Welcome to my program.\n";
-
-    cout << total << endl;
-    cout << percentage << endl;
+    
+    cout << round(percentageMales) << '%' << endl;
+    cout << round(percentageFemales) << '%' << endl;
 
     return 0;
 }
