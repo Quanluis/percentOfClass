b540ecfd95fa6b1b5f5d3be533f328c8bb705ef8
Quanluis <quanluis@protonmail.com>
2019-08-22 14:14:47 -0400

    included 2 decimal points

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main(){
@@ -7,16 +8,16 @@ int main(){
     // Number of students in a class based on gender.
     // Change values if needed (only change male and female variables)
 
-    double males = 25,
-        females = 25,
+    double males = 35,
+        females = 23,
         total = males + females,
         percentageMales = (float)males / (float)total * 100, 
         percentageFemales = (float)females / (float)total * 100;
 
     cout << "Welcome to my program.\n";
     
-    cout << round(percentageMales) << '%' << endl;
-    cout << round(percentageFemales) << '%' << endl;
+    cout << setprecision(4) << percentageMales  << '%' << endl;
+    cout << setprecision(4) << percentageFemales  << '%' << endl;
 
     return 0;
 }
